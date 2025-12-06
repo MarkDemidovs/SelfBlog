41c05a19fd71649961b73482442d3f7be394d9e0
Mark Demidovs <markdemidovs@proton.me>
2025-12-06 17:12:53 +0200

    Secure handling of POST and DELETE.

--- backend/blogRoutes.js
+++ backend/blogRoutes.js
@@ -2,10 +2,17 @@ const express = require("express");
 const router = express.Router();
 const blogController = require("./blogController.js");
 
+const protectRoutes = (req, res, next) => {
+  const key = req.headers["x-api-key"];
+  if (!key || key !== process.env.API_KEY) {
+    return res.status(403).json({ error: "Forbidden" });
+  }
+  next();
+};
+
 router.get("/", blogController.getAllArticles);
 
-router.post("/", blogController.createArticle);
-
-router.delete("/:id", blogController.deleteArticle);
+router.post("/", protectRoutes, blogController.createArticle);
+router.delete("/:id", protectRoutes, blogController.deleteArticle);
 
 module.exports = router;
